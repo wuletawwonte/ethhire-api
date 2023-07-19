7bfafb81e4c88110fefdc39e4cd5c11f4b2fcbdb
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-07-19 11:54:50 +0300

    Fix rubocop Linter errors

--- test/controllers/jobs_controller_test.rb
+++ test/controllers/jobs_controller_test.rb
@@ -1,35 +1,37 @@
-require "test_helper"
+require 'test_helper'
 
 class JobsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @job = jobs(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get jobs_url, as: :json
     assert_response :success
   end
 
-  test "should create job" do
-    assert_difference("Job.count") do
-      post jobs_url, params: { job: { description: @job.description, location: @job.location, salary: @job.salary, title: @job.title, type: @job.type } }, as: :json
+  test 'should create job' do
+    assert_difference('Job.count') do
+      post jobs_url,
+           params: { job: { description: @job.description, location: @job.location, salary: @job.salary, title: @job.title, type: @job.type } }, as: :json
     end
 
     assert_response :created
   end
 
-  test "should show job" do
+  test 'should show job' do
     get job_url(@job), as: :json
     assert_response :success
   end
 
-  test "should update job" do
-    patch job_url(@job), params: { job: { description: @job.description, location: @job.location, salary: @job.salary, title: @job.title, type: @job.type } }, as: :json
+  test 'should update job' do
+    patch job_url(@job),
+          params: { job: { description: @job.description, location: @job.location, salary: @job.salary, title: @job.title, type: @job.type } }, as: :json
     assert_response :success
   end
 
-  test "should destroy job" do
-    assert_difference("Job.count", -1) do
+  test 'should destroy job' do
+    assert_difference('Job.count', -1) do
       delete job_url(@job), as: :json
     end
 
--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class JobTest < ActiveSupport::TestCase
   # test "the truth" do
