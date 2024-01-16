fc25e3879a459b68d35c258b41ad86958c89e972
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-01-16 11:17:49 +0300

    fix controller test errors

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -6,31 +6,31 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
-    get categories_url, as: :json
+    get api_v1_categories_url, as: :json
     assert_response :success
   end
 
   test "should create category" do
     assert_difference("Category.count") do
-      post categories_url, params: {category: {name: @category.name}}, as: :json
+      post api_v1_categories_url, params: {category: {name: @category.name}}, as: :json
     end
 
     assert_response :created
   end
 
   test "should show category" do
-    get category_url(@category), as: :json
+    get api_v1_category_url(@category), as: :json
     assert_response :success
   end
 
   test "should update category" do
-    patch category_url(@category), params: {category: {name: @category.name}}, as: :json
+    patch api_v1_category_url(@category), params: {category: {name: @category.name}}, as: :json
     assert_response :success
   end
 
   test "should destroy category" do
     assert_difference("Category.count", -1) do
-      delete category_url(@category), as: :json
+      delete api_v1_category_url(@category), as: :json
     end
 
     assert_response :no_content
--- test/controllers/jobs_controller_test.rb
+++ test/controllers/jobs_controller_test.rb
@@ -6,33 +6,33 @@ class JobsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
-    get jobs_url, as: :json
+    get api_v1_jobs_url, as: :json
     assert_response :success
   end
 
   test "should create job" do
     assert_difference("Job.count") do
-      post jobs_url,
-        params: {job: {description: @job.description, location: @job.location, salary: @job.salary, title: @job.title, type: @job.type}}, as: :json
+      post api_v1_jobs_url,
+        params: {job: {description: @job.description, location: @job.location, salary: @job.salary, title: @job.title, job_type: @job.job_type}}, as: :json
     end
 
     assert_response :created
   end
 
   test "should show job" do
-    get job_url(@job), as: :json
+    get api_v1_job_url(@job), as: :json
     assert_response :success
   end
 
   test "should update job" do
-    patch job_url(@job),
-      params: {job: {description: @job.description, location: @job.location, salary: @job.salary, title: @job.title, type: @job.type}}, as: :json
+    patch api_v1_job_url(@job),
+      params: {job: {description: @job.description, location: @job.location, salary: @job.salary, title: @job.title, job_type: @job.job_type}}, as: :json
     assert_response :success
   end
 
   test "should destroy job" do
     assert_difference("Job.count", -1) do
-      delete job_url(@job), as: :json
+      delete api_v1_job_url(@job), as: :json
     end
 
     assert_response :no_content
