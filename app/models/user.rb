2c5c5c8877013272cd705a8714ddde18fedcea33
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-01-28 11:41:55 +0300

    add api configuration in devise initializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :api
+    :recoverable, :rememberable, :validatable, :api
 
   validate :first_name, :last_name, :email, presence: true
 
