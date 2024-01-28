3a1379060b93f22750b9c39ebf2f67dbf875ac0f
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-01-28 11:38:30 +0300

    add validation for User model attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,5 +22,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :validatable, :api
+         :recoverable, :rememberable, :validatable, :api
+
+  validate :first_name, :last_name, :email, presence: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
